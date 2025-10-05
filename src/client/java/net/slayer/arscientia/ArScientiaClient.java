18facf64b4848a5db7bdb5da828bfe3e3af9ec82
SlayeRRROAR <83496342+SlayeRRROAR@users.noreply.github.com>
2025-10-05 16:06:40 +0200

    Initial commit

---
+++ src/client/java/net/slayer/arscientia/ArScientiaClient.java
@@ -0,0 +1,11 @@
+package net.slayer.arscientia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArScientiaClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
